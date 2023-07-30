bd31b5944ae4d0c9c4a4188cbbb205654e67245e
Marios_P <pliatsikasmarios@gmail.com>
2023-07-30 22:57:15 +0200

    Initial commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by MARIOS PLIATSIKAS on 20.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        //VStack {
+            //Image(systemName: "globe")
+               // .imageScale(.large)
+                //.foregroundColor(.accentColor)
+        
+        
+        Text("Hello, world!").foregroundColor(Color.orange) 
+            
+            
+            
+                
+        }
+        
+    }
+//}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by MARIOS PLIATSIKAS on 20.07.23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
